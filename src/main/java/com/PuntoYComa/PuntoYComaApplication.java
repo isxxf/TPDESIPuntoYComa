030e8da4fceee2f53aec9b606ac0243fb6208fd7
Isa <103289418+isxxf@users.noreply.github.com>
2026-06-23 18:43:44 -0300

    Initial commit

---
+++ src/main/java/com/PuntoYComa/PuntoYComaApplication.java
@@ -0,0 +1,13 @@
+package com.PuntoYComa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoYComaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoYComaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PuntoYComa/PuntoYComaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PuntoYComa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoYComaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
