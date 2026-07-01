aff9e1a7581a138eb4db2add8f363fb8a7062d0c
ESTEBAN BRUNO CASTRO <estebanbrunocastro@MacBook-Air-de-ESTEBAN.local>
2026-07-01 02:37:29 -0400

    feat: add Ciudad repository and services

--- src/main/java/PuntoYComa/presentacion/PropiedadController.java
+++ src/main/java/PuntoYComa/presentacion/PropiedadController.java
@@ -1,6 +1,8 @@
 package PuntoYComa.presentacion;
 
 import PuntoYComa.entidades.Propiedad;
+import PuntoYComa.servicios.CiudadService;
+import PuntoYComa.servicios.PersonaService;
 import PuntoYComa.servicios.PropiedadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,12 @@ public class PropiedadController {
 
     @Autowired
     private PropiedadService propiedadService;
+    
+    @Autowired
+    private CiudadService ciudadService;
+
+    @Autowired
+    private PersonaService personaService;
 
     @GetMapping
     public String listarPropiedades(Model model) {
@@ -25,30 +33,54 @@ public class PropiedadController {
 
     @GetMapping("/nuevo")
     public String mostrarFormulario(Model model) {
+
         model.addAttribute("propiedad", new Propiedad());
-        // agregar  lista de personas, ciudades, etc.
+        model.addAttribute("ciudades", ciudadService.listar());
+        model.addAttribute("personas", personaService.listar());
+
         return "propiedades/formulario";
     }
 
+    
     @PostMapping("/guardar")
     public String guardarPropiedad(@ModelAttribute Propiedad propiedad) {
+
+        System.out.println("Ciudad ID: " + propiedad.getCiudad().getId());
+        System.out.println("Propietario ID: " + propiedad.getPropietario().getId());
+        System.out.println(propiedad.getCiudad());
+
+        if (propiedad.getCiudad() != null) {
+            System.out.println(propiedad.getCiudad().getId());
+        }
+
         propiedadService.guardar(propiedad);
+
         return "redirect:/propiedades";
     }
 
     @GetMapping("/editar/{id}")
     public String editarPropiedad(@PathVariable Long id, Model model) {
+
         Propiedad propiedad = propiedadService.buscarPorId(id);
+
         model.addAttribute("propiedad", propiedad);
-        //  agregar  listas de personas, ciudades, etc.
+        model.addAttribute("ciudades", ciudadService.listar());
+        model.addAttribute("personas", personaService.listar());
+
         return "propiedades/formulario";
     }
 
     @PostMapping("/actualizar")
     public String actualizarPropiedad(@ModelAttribute Propiedad propiedad) {
+
+        System.out.println("ID = " + propiedad.getId());
+
         propiedadService.actualizar(propiedad);
+
         return "redirect:/propiedades";
     }
+
+    
 
     @GetMapping("/eliminar/{id}")
     public String eliminarPropiedad(@PathVariable Long id) {
